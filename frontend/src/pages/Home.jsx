c822e8baeb0be2ed5e71d34b87b2752c1b12d37d
mdshakibsami <mdshakibsami21@gmail.com>
2025-06-30 00:25:01 +0600

    change favicons and titile

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import Banner from "../components/Banner/Banner";
 import About from "../components/About/About";
 import Technologies from "../components/Technologies/Technologies";
 import Contact from "../components/Contact/Contact";
-import Experience from "../components/Experience/Experience";
 import Projects from "../components/Projects/Projects";
+import Experience from "../components/Experience/Experience";
 
 const Home = () => {
   return (
